c450b3572ba3bd935c8a3d0aef1063241759d8fb
Valentin Popescu <ppsc.valentin@gmail.com>
2016-06-29 16:35:17 +0300

    update grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,48 +1,42 @@
 module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
-  grunt.initConfig({
-    less: {
-      options: {
-        compress: false,
-        yuicompress: true,
-        optimization: 2,
-      },
-      development: {
-        options: {
-          sourceMap: true,
-          sourceMapFileInline: true
-          // sourceMapFilename: "css/main.css.map",
-          // sourceMapBasepath: "css/",
-          // sourceMapURL: "main.css.map"
-        },
-        files: {
-          './static/css/main.css': './src/less/main.less'
-        }
-      }
-    },
-    postcss: {
-      options: {
-        map: true,
-        processors: [
-          require('autoprefixer')()
-        ]
-      },
-      dist: {
-        src: './static/css/main.css'
-      }
-    },
-    watch: {
-      styles: {
-        files: ['./src/less/**/*.less'], // which files to watch
-        tasks: ['less:development', 'postcss'],
-        options: {
-          nospawn: true
-        }
-      }
-    }
+
+  // Config
+  var merge = require('merge'),
+      config = {};
+
+  config.path = {
+    'static': 'static',
+    'src': 'src',
+    'node': 'node_modules'
+  };
+
+  [
+    require('./grunt/development.js'),
+    require('./grunt/production.js')
+  ].forEach(function(settings) {
+    config = merge.recursive(true, config, settings);
   });
 
-  grunt.registerTask('default', ['less:development', 'postcss', 'watch']);
-  grunt.registerTask('dist', ['less:production', 'postcss']);
+  grunt.initConfig(config);
+
+
+  // Tasks
+  grunt.registerTask('development', [
+    'less:development',
+    'concat:js'
+  ]);
+
+  grunt.registerTask('production', [
+    'less:production',
+    'copy',
+    'uglify',
+    'postcss'
+  ]);
+
+  grunt.registerTask('default', [
+    'development',
+    'watch'
+  ]);
 };
---
+++ grunt/development.js
@@ -0,0 +1,53 @@
+module.exports = {
+  less: {
+    development: {
+      options: {
+        compress: false,
+        sourceMap: true,
+        sourceMapFilename: '<%= path.static %>/css/source.css.map',
+        sourceMapURL: './source.css.map'
+      },
+      files: {
+        '<%= path.static %>/css/main.css': '<%= path.src %>/less/main.less'
+      }
+    }
+  },
+  concat: {
+    js: {
+      options: {
+        separator: ';'
+      },
+      src: [
+        '<%= path.src %>/js/contrib/bootstrap.collapse.js'
+      ],
+      dest: '<%= path.static %>/js/main.js',
+    }
+  },
+  // copy: {
+  //   scripts: {
+  //     files: [
+  //       { expand: true,
+  //         cwd: '<%= path.src%>/js/',
+  //         src: ['map.js'],
+  //         dest: '<%= path.static %>/js/'
+  //       }
+  //     ]
+  //   }
+  // },
+  watch: {
+    styles: {
+      files: ['<%= path.src %>/less/**/*.less'],
+      tasks: ['less:development'],
+      options: {
+        nospawn: true
+      }
+    },
+    scripts: {
+      files: ['<%= path.src %>/js/**/*.js'],
+      tasks: ['concat'],
+      options: {
+        nospawn: true
+      }
+    }
+  }
+}
